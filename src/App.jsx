ac35ac4f18fc3188a43b02019337bd6ce8b8c913
Keshav <keshav22.dlp@gmail.com>
2025-01-25 00:29:21 +0530

    Make conf file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
 
   return (
     <>
       <h1>megaBlog</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
